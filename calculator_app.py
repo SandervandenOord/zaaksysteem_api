07b66bd0050e29eadc0f2f9c3613ae143c775478
Sander van den Oord <salieri78@hotmail.com>
2019-02-14 13:15:11 +0100

    small changes

--- calculator_app.py
+++ calculator_app.py
@@ -6,7 +6,7 @@ from resources.calculator import Calculator
 
 app = Flask(__name__)
 
-api = Api(app, prefix='/api/v1', version='0.1')
+api = Api(app, prefix='/api/v1', doc='/api/v1', version='0.1',)
 
 api.add_resource(
     Calculator,
