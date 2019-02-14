db86f1a21d51470b940d4a4c0d697dfe03015b67
Sander van den Oord <salieri78@hotmail.com>
2019-02-14 13:22:29 +0100

    calculator returns dictionary

--- resources/calculator.py
+++ resources/calculator.py
@@ -5,10 +5,10 @@ from flask_restplus import Resource
 class Calculator(Resource):
     def get(self, calculator_input):
         """
-        Take an input string and calculate its result with a simple calculator
+        Take an input string and calculate its result with a simple calculator.
+        The input should contain spaces between numbers and operators.
         """
         calculator = SimpleCalculator()
         calculator.run(calculator_input)
-        result_of_calculation = calculator.reg_1
 
-        return result_of_calculation
+        return {'result_of_calculation': calculator.reg_1}
