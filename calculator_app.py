f0707fe4e13993a3c8c5455b0cbb92814918cad2
Sander van den Oord <salieri78@hotmail.com>
2019-02-14 10:27:04 +0100

    added calculator endpoint

--- calculator_app.py
+++ calculator_app.py
@@ -2,29 +2,17 @@ from flask import Flask
 from flask_restful import Api
 
 from local_settings import HOST, PORT
-from resources.calculator import Calculator  # , calculator_api
+from resources.calculator import Calculator
 
 app = Flask(__name__)
 
 api = Api(app)
 
-api.add_resource(Calculator, '/calculator')
-# app.register_blueprint(calculator_api)
-
-
-# NOG IETS DOEN MET reqparse? om te kijken of arguments valide zijn!?!
-# @app.route('/calculator/<string:calculator_input>', methods=['GET'])
-# def calculate(calculator_input):
-#     """
-#     Take an input string and calculate its result with a simple calculator
-#     """
-#     print(calculator_input)
-#     calculator = SimpleCalculator()
-#     calculator.run(calculator_input)
-#     print(calculator.log)
-#     result = calculator.reg_1
-#
-#     return jsonify(result)
+api.add_resource(
+    Calculator,
+    '/calculator/<string:calculator_input>',
+    endpoint='calculator',
+)
 
 
 if __name__ == '__main__':
--- resources/calculator.py
+++ resources/calculator.py
@@ -1,23 +1,14 @@
-# from calculator.simple import SimpleCalculator
-# from flask import Blueprint
-# from flask_restful import Resource, Api
-from flask_restplus import Resource # , Api
+from calculator.simple import SimpleCalculator
+from flask_restplus import Resource
 
 
 class Calculator(Resource):
-    def get(self):
-        return 'Test'
+    def get(self, calculator_input):
+        """
+        Take an input string and calculate its result with a simple calculator
+        """
+        calculator = SimpleCalculator()
+        calculator.run(calculator_input)
+        result_of_calculation = calculator.reg_1
 
-
-# calculator_api = Blueprint(name='resources.calculator', import_name=__name__)
-# api = Api(calculator_api)
-# api.add_resource(
-#     resource=Calculator,
-#     urls='/calculator',
-#     # endpoint='calculator',
-# )
-#
-# print(api)
-# print(dir(calculator_api))
-# print(calculator_api.__dict__)
-# print(api.__dict__)
+        return result_of_calculation
